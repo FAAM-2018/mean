f0b9394f88b7b05309e532e2bf861e8bfb339d72
Anh Nguyen <anhknguyen97@yahoo.com>
2018-09-19 17:03:37 -0700

    Adding anh's method

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -237,8 +237,9 @@ module.exports = function (options) {
         // app.get('/some/path', function(req, res) {
         //   res.json({ custom: 'response' });
         // });
-	app.get('/alex', (req,res)=> res.send('it\'s anh'))
+	app.get('/alex', (req,res)=> res.send('it\'s alex'))
 	app.get('/jmrv', (req,res)=> res.send('it\'s marco'))
+	app.get('/anh', (req,res)=> res.send('it\'s anh'))
       },
       proxy: {
         '/api':  `http://localhost:${process.env.SERVER_PORT || '4040'}`
